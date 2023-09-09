5f34920738e5abd85cb29413dba7a0843895676d
Carlos Perales <carlosperales050@gmail.com>
2023-09-08 19:32:41 -0600

    consumo de pokeApi

--- src/screen/PokeDex.js
+++ src/screen/PokeDex.js
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
 import { Text, SafeAreaView } from "react-native";
-
+import { getPokesApi } from "../api/pokemon";
 export default function PokeDex() {
+  const { data, setData } = useState([]);
+
+  useEffect(() => {
+    loadPokes();
+  }, []);
+
+  const loadPokes = async () => {
+    try {
+      const data = await getPokesApi();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <SafeAreaView>
-      <Text>PokeDex</Text>
+      <Text> POKEDEX</Text>
     </SafeAreaView>
   );
 }
